8bf13cbcda9c18662b397b71640926a9d2e31243
David Megginson <david.megginson@gmail.com>
2020-03-20 16:30:15 -0400

    Check if the zipfile is really an Excel XLSX file

--- tests/test_workbook.py
+++ tests/test_workbook.py
@@ -14,13 +14,17 @@ from . import resolve_path
 
 class TestWorkbook(unittest.TestCase):
 
-    def test_open_workbook_filename(self):
+    def test_open_workbook_from_filename(self):
         xlsxr.Workbook(filename=resolve_path("simple.xlsx"))
         self.assertTrue(True)
 
-    def test_open_workbook_stream(self):
+    def test_open_workbook_from_stream(self):
         with open(resolve_path("simple.xlsx"), "rb") as input:
             xlsxr.Workbook(stream=input)
 
-    def test_open_workbook_url(self):
+    def test_open_workbook_from_url(self):
         xlsxr.Workbook(url="https://github.com/davidmegginson/xlsx-reader/blob/master/tests/files/simple.xlsx?raw=true")
+
+    def test_open_non_excel_archive(self):
+        with self.assertRaises(TypeError):
+            xlsxr.Workbook(filename=resolve_path("not-excel.zip"))
--- xlsxr/workbook.py
+++ xlsxr/workbook.py
@@ -21,7 +21,6 @@ class Workbook:
         @param filename: path to an Excel file on the local system.
         @param stream: file-like object (byte stream)
         @param url: web address of a remote Excel file
-
         """
 
         if filename is not None:
@@ -36,7 +35,17 @@ class Workbook:
             self.archive = zipfile.ZipFile(tmpfile)
         else:
             raise ValueError("Must specify filename, stream, or url argument")
+
+        self.setup() # will throw an exception if it's not an XLSX file
             
 
-            
+    def setup (self):
+        """ Set up the workbook 
+        @raises TypeError: if the zip file is not an XLSX file
+        """
+        for item in self.archive.infolist():
+            if item.filename == "xl/workbook.xml":
+                self.workbook_item = item
+                return
+        raise TypeError("Zip archive is not an Excel XLSX file")
             
