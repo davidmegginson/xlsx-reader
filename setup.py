0613b179e5a3ccefa2e168b49e692eb073c93a61
David Megginson <david.megginson@gmail.com>
2022-12-03 10:54:12 -0500

    Change name to 'xlsxr'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3,):
     raise RuntimeError("xlsx-reader requires Python 3 or higher")
 
 setup(
-    name='xlsx-reader',
+    name='xlsxr',
     version="0.1",
     description="Read very large Excel XLSX files efficiently",
     author='David Megginson',
